9d1c1e009f0f9beb2c75af00b0736d1a11ca5974
roy yoo <royyoo123@gmail.com>
2021-02-22 15:24:29 -0800

    seeded database and fixed some association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events, through: :attendances
+  has_many :events
   has_many :attendances
   validates :email, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+	user = User.create!(email: Faker::Internet.email, password: "password")
+	Event.create!(name: Faker::Restaurant.name, price: rand(1..400), address: Faker::Address.street_address, user: user)
+end
