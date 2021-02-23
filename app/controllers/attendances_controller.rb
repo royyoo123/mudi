20b1887e7e5daff69351609044638eb853897d7a
roy yoo <royyoo123@gmail.com>
2021-02-22 16:08:46 -0800

    controller done

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,15 +1,10 @@
 class AttendancesController < ApplicationController
 	def new
-<<<<<<< HEAD
-
 		@event = Event.find(params[:event_id])
-=======
->>>>>>> d9263db294cac52adf20a948a3d9b01498b2ef5a
 		@attendance = Attendance.new
 	end
 
 	def create
-<<<<<<< HEAD
 		@user = current_user
 		@event = Event.find(params[:event_id])
 		@attendance.user = @user
@@ -19,13 +14,7 @@ class AttendancesController < ApplicationController
 
 	  redirect_to event_path
 	end
-=======
 		@attendance = Attendance.new(attendance_params)
 		@attendance.save
-
-	
 	end
-
-	
->>>>>>> d9263db294cac52adf20a948a3d9b01498b2ef5a
 end
