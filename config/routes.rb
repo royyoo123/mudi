46b6d9fb6ceb47b227cf1fed6f7ae8c8d6ee4645
roy yoo <royyoo123@gmail.com>
2021-02-24 09:05:31 -0800

    route configured a little

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#home'
+  resources :users, only: [] do
+  	resources :attendances, only: [:new, :create, :show, :index]
+  end
+
   resources :events, only: [:show, :index, :new, :create]
-  resources :attendances, only: [:new, :create, :show, :index]
+  # resources :attendances, only: [:new, :create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
