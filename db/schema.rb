2ffde5f808e00fa062fb29d97704bc5fc54e85e6
roy yoo <royyoo123@gmail.com>
2021-02-25 12:38:29 -0800

    datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_003242) do
+ActiveRecord::Schema.define(version: 2021_02_25_203027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_003242) do
     t.float "longitude"
     t.date "date"
     t.time "time"
+    t.datetime "start_date"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
