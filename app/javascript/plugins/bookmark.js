f8fc48f2a10dceea752bbfaad5fb766787cd7707
roy yoo <royyoo123@gmail.com>
2021-03-01 09:21:34 -0800

    a

--- app/javascript/plugins/bookmark.js
+++ app/javascript/plugins/bookmark.js
@@ -1,5 +1,5 @@
-let bookmark = () => {
-	let hearts = document.querySelectorAll(".bookmark");
+const bookmark = () => {
+	const hearts = document.querySelectorAll(".bookmark");
 	hearts.forEach((heart) => {
 		heart.addEventListener("click", (event) => {
 			console.log("test")
