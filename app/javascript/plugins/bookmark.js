8a5f5365340c7d79561ec973f164f579cf50236d
roy yoo <royyoo123@gmail.com>
2021-03-03 15:21:13 -0800

    remote true

--- app/javascript/plugins/bookmark.js
+++ app/javascript/plugins/bookmark.js
@@ -2,11 +2,12 @@ const bookmark = () => {
 	const hearts = document.querySelectorAll(".bookmark-heart");
 	hearts.forEach((heart) => {
 		heart.addEventListener("click", (event) => {
+			event.preventDefault()
+		
 			heart.classList.toggle("far")
 			heart.classList.toggle("fas")
 			heart.classList.toggle("red")
 			const eventId = heart.dataset.formId
-			event.preventDefault();
 			const formContainer = document.querySelector(`#form_${eventId}`)
 			if (formContainer) {
 			formContainer.querySelector('form').submit()
