10064bcd4e09cc5fd14fb69e94ffef00917f3908
roy yoo <royyoo123@gmail.com>
2021-03-05 10:31:11 -0800

    fixed bug, added events with no date to calendar query

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,13 +11,9 @@ class EventsController < ApplicationController
 		    @events = Event.near(coords, 50)
 		  end
 	    if params[:query].present?
-	    	if @events
-	    		@events = @events.search_by_name_and_description(params[:query])
-	    	else
-	    		@events = Event.search_by_name_and_description(params[:query])
-	    	end	
+	    	@events ? @events.search_by_name_and_description(params[:query]) : Event.search_by_name_and_description(params[:query])
 	    end
-	   
+	    @events = Event.all if @events.nil?
     else
     	date_time=params[:date].to_datetime
     	if params[:latitude].present? && params[:longitude].present?
@@ -25,13 +21,12 @@ class EventsController < ApplicationController
 		    longitude = params[:longitude]
 		    coords = [latitude,longitude]
 		    # @events = Event.near(coords, 50).where('start_date= date_time')
-		    @events = Event.near(coords, 50).where("start_date > ?", date_time)
+		    @events = Event.near(coords, 50).where("(start_date BETWEEN ? AND ?) OR start_date IS NULL", date_time.beginning_of_day, date_time.end_of_day)
 	    else
-	    	@events = Event.where("start_date > ?", date_time)
+	    	@events = Event.where("(start_date BETWEEN ? AND ?) OR start_date IS NULL", date_time.beginning_of_day, date_time.end_of_day)
 	    	# @events = Event.where.not("start_date: ?", nil)
 	    end
     end
- 
     @events = policy_scope(@events)
 	end
 
