b035b038d55078a5aa4cf9ba843eee48da04ad4e
roy yoo <royyoo123@gmail.com>
2021-03-07 23:03:40 -0800

    refactoring

--- app/javascript/plugins/geolocator.js
+++ app/javascript/plugins/geolocator.js
@@ -9,13 +9,14 @@ const locateCoords = () => {
 	const b = document.getElementById('map-button1');
 	const c = document.querySelector('.btn-map');
 	const d = document.getElementById('back-btn');
+
 	const handleButtonClick = (event) => {
 		window.location.href=`http://www.localhost:3000/events`;
 	}
 	if (a) a.addEventListener('click', handleButtonClick)
 	function success(pos) {
 	  var crd = pos.coords;
-	  if(a){
+	  if(a || c || d){
 			a.removeEventListener('click', handleButtonClick)
 	  	a.addEventListener('click',(event) => {
 			window.location.href=`http://www.localhost:3000/events?latitude=${crd.latitude}&longitude=${crd.longitude}`;
@@ -24,15 +25,7 @@ const locateCoords = () => {
 			b.addEventListener('click',(event) => {
 			window.location.href=`http://www.localhost:3000/events/map?latitude=${crd.latitude}&longitude=${crd.longitude}`;
 			})
-		} else if (c) {
-			c.addEventListener('click',(event)=> {
-			window.location.href=`http://www.localhost:3000/events?latitude=${crd.latitude}&longitude=${crd.longitude}`;
-			})
-		} else if (d) {
-			d.addEventListener('click',(event)=> {
-			window.location.href=`http://www.localhost:3000/events?latitude=${crd.latitude}&longitude=${crd.longitude}`;
-			})
-		}
+		} 
 	}
 	function error(err) {
 	  console.warn(`ERROR(${err.code}): ${err.message}`);
