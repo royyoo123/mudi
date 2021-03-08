6556f5a6ebe4401dfa846c8837696c2ee3a0d37f
roy yoo <royyoo123@gmail.com>
2021-03-08 12:28:21 -0800

    a

--- app/javascript/plugins/bookmark.js
+++ app/javascript/plugins/bookmark.js
@@ -3,7 +3,7 @@ const bookmark = () => {
 	console.log();
 	hearts.forEach((heart) => {
 		const eventId = heart.dataset.formId;
-		heart.addEventListener("touchstart click", (event) => {
+		heart.addEventListener("touchstart", (event) => {
 			event.preventDefault();
 			// console.log(event.path[0].className);
 			if(event.path[0].className === "fas fa-bookmark"){
