3bb5ff9bf3eb4b5082fb164a71976d18a29efdbe
roy yoo <royyoo123@gmail.com>
2021-03-08 10:27:07 -0800

    a

--- app/javascript/plugins/bookmark.js
+++ app/javascript/plugins/bookmark.js
@@ -42,6 +42,44 @@ const bookmark = () => {
 			// }
 
 		});
+		heart.addEventListener("touchstart", (event) => {
+			event.preventDefault();
+			// console.log(event.path[0].className);
+			if(event.path[0].className === "fas fa-bookmark"){
+				fetch(`/bookmarks/${eventId}`, {
+					method: "DELETE"
+					// body: JSON.stringify({})
+				})
+				.then((data)=>{
+					heart.classList.toggle("fas")
+					heart.classList.toggle("far")
+				});
+			} else {
+				const data = {
+					bookmark: { event_id: eventId }
+				}
+				fetch(`/bookmarks`, {
+					method: "POST",
+					headers: {"Content-Type": "application/json"},
+					body: JSON.stringify(data)
+				})
+
+				.then((data) => {
+					heart.classList.toggle("fas")
+					heart.classList.toggle("far")
+				});
+			}
+			// console.log('hello');
+			// heart.classList.toggle("far");
+			// heart.classList.toggle("fas");
+			// heart.classList.toggle("red");
+			// const eventId = heart.dataset.formId;
+			// const formContainer = document.querySelector(`#form_${eventId}`);
+			// if (formContainer) {
+			// 	// formContainer.querySelector('form').submit();
+			// }
+
+		});
 	});
 
 };
