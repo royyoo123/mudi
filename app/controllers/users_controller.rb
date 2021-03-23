a29bbf08d48a29ee833d1aa415b8dbad189152b4
roy yoo <royyoo123@gmail.com>
2021-03-22 19:55:29 -0700

    detecting device variant for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
 	skip_before_action :authenticate_user!, only: :home
 	skip_after_action :verify_authorized, only: :home
+	protect_from_forgery with: :exception
+  before_action :detect_device_variant
 	def home
 		@moods = Mood.all
 	end
+
+	private
+	def detect_device_variant
+    request.variant = :phone if browser.device.mobile?
+  end
 end
